f9df3ef894c763fde228162d063fcde76cc18e71
deshondixon <deshondixon@gmail.com>
2022-11-16 17:13:23 -0800

    lab7 fix

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 
 console.log('our first server');
 
-
 const express = require('express');
 let data = require('./data/weather.json');
-
-
 
 require('dotenv').config();
 
@@ -15,32 +12,22 @@ const { response } = require('express');
 
 const app = express();
 
-
 app.use(cors());
 
-
-
 const PORT = process.env.PORT || 3002;
-
 
 app.get('/', (request, response) => {
   response.send('Hello, from my server');
 });
 
-
 app.get('/weather', (request, response, next) => {
   try {
-    let city = request.query.cityName;
+    let cityName = request.query.city;
+    let selectedCity = data.find((city) => city.city_name === cityName);
 
-    let selectedCity = data.find(object => object.city_name === city);
-
-    let cityCleanedUp = [];
-
-    for(let i = 0; i < selectedCity.data.length;i++) {
-      cityCleanedUp.push(new Forecast(selectedCity.data[i]));
-    }
+    let forecastArr = selectedCity.data.map((day) => new Forecast(day));
+    response.send(forecastArr);
   } catch (error) {
-
     next(error);
   }
 });
@@ -53,11 +40,10 @@ app.get('*', (request, response) => {
   response.send('That route does not exist');
 });
 
-
 class Forecast {
-  constructor(weatherDay) {
-    this.date = weatherDay.valid_date;
-    this.description = weatherDay.weather.description;
+  constructor(forecastObject) {
+    this.date = forecastObject.valid_date;
+    this.description = forecastObject.weather.description;
   }
 }
 
