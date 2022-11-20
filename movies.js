b9ffe0827a7e06635a8deadff9cd237e9a7dc090
deshondixon <deshondixon@gmail.com>
2022-11-20 13:17:44 -0800

    refactor2

--- movies.js
+++ movies.js
@@ -1,25 +1,18 @@
 const axios = require('axios');
 
-async function getMovies(request, response, next) {
-  try {
-    let searchValue = request.query.search;
+async function getMovies(request, response) {
+  let searchValue = request.query.search;
 
-    let params = {
-      api_key: process.env.MOVIE_API_KEY,
-      query: searchValue,
-    };
-    let baseURL = 'http://api.themoviedb.org/3/search/movie';
-    let results = await axios.get(baseURL, { params });
+  let params = {
+    api_key: process.env.MOVIE_API_KEY,
+    query: searchValue,
+  };
+  let baseURL = 'http://api.themoviedb.org/3/search/movie';
 
-    let moviesArr = results.data.results.map((movie) => new Movie(movie));
-    response.send(moviesArr);
-  } catch (error) {
-    Promise.resolve()
-      .then(() => {
-        throw new Error(error.message);
-      })
-      .catch(next);
-  }
+  axios.get(baseURL, { params })
+    .then((results) => results.data.results.map((movie) => new Movie(movie)))
+    .then((moviesArr) => response.send(moviesArr))
+    .catch((error) => console.log(error));
 }
 
 //CLASS
--- weather.js
+++ weather.js
@@ -1,27 +1,21 @@
 const axios = require('axios');
 
-async function getWeather(request, response, next) {
-  try {
-    let weatherLat = request.query.weatherLat;
-    let weatherLon = request.query.weatherLon;
+async function getWeather(request, response) {
+  let weatherLat = request.query.weatherLat;
+  let weatherLon = request.query.weatherLon;
 
-    let params = {
-      key: process.env.WEATHER_API_KEY,
-      days: 3,
-      lat: weatherLat,
-      lon: weatherLon,
-    };
-    let baseURL = 'http://api.weatherbit.io/v2.0/forecast/daily';
-    let results = await axios.get(baseURL, { params });
-    let forecastArr = results.data.data.map((day) => new Forecast(day));
-    response.send(forecastArr);
-  } catch (error) {
-    Promise.resolve()
-      .then(() => {
-        throw new Error(error.message);
-      })
-      .catch(next);
-  }
+  let params = {
+    key: process.env.WEATHER_API_KEY,
+    days: 3,
+    lat: weatherLat,
+    lon: weatherLon,
+  };
+  let baseURL = 'http://api.weatherbit.io/v2.0/forecast/daily';
+
+  axios.get(baseURL, { params })
+    .then((results) => results.data.data.map((day) => new Forecast(day)))
+    .then((forecastArr) => response.send(forecastArr))
+    .catch((error) => console.log(error));
 }
 
 // CLASS
